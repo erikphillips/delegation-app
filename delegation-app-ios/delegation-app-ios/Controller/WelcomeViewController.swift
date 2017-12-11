9707fcbbbdc851d95cebb4b0b3f4ab09010c0dd9
Erik Phillips <erik.phillips@icloud.com>
2017-12-10 18:20:46 -0800

    added templates for sign-in and account creation storyboards

---
+++ delegation-app-ios/delegation-app-ios/Controller/WelcomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WelcomeViewController.swift
+//  delegation-app-ios
+//
+//  Created by Erik Phillips on 12/10/17.
+//  Copyright © 2017 Erik Phillips. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
