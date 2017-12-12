232849e86ad06d3a05a9d4423cedd9f4800dba05
Erik Phillips <erik.phillips@icloud.com>
2017-12-11 20:35:14 -0800

    Initial commit of macOS app

---
+++ delegation-app-macos/delegation-app-macos/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  delegation-app-macos
+//
+//  Created by Erik Phillips on 12/11/17.
+//  Copyright © 2017 Erik Phillips. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
