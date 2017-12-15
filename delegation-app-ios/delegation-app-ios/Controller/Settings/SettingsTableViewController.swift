75664b9e519d45107529f41f2a9145295a65fd83
Erik Phillips <erik.phillips@icloud.com>
2017-12-15 11:49:23 -0700

    added tasks table and settings redone

--- delegation-app-ios/delegation-app-ios/Controller/Settings/SettingsTableViewController.swift
+++ delegation-app-ios/delegation-app-ios/Controller/Settings/SettingsTableViewController.swift
@@ -9,91 +9,47 @@
 import UIKit
 
 class SettingsTableViewController: UITableViewController {
-
+    
+    @IBOutlet weak var settingsTableView: UITableView!
+    @IBAction func settingsLogout(_ sender: Any) {
+        self.performSegue(withIdentifier: "unwindToWelcomeView", sender: sender)
+    }
+    
+    @IBOutlet weak var settingsFirstname: UILabel!
+    @IBOutlet weak var settingsLastname: UILabel!
+    @IBOutlet weak var settingsEmailAddress: UILabel!
+    @IBOutlet weak var settingsPhoneNumber: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("Settings TableView loaded.")
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        settingsFirstname.text = "<first_name>"
+        settingsLastname.text = "<last_name>"
+        settingsEmailAddress.text = "<email_address>"
+        settingsPhoneNumber.text = "<phone_number>"
     }
     
-    @IBAction func settingsLogout(_ sender: Any) {
-        self.performSegue(withIdentifier: "unwindToWelcomeView", sender: sender)
-    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
-
-    /*
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "<TODO: account_fullname>"
+        case 1:
+            return "Teams"
+        default:
+            return nil
+        }
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-    */
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "unwindToWelcomeView" {
             print("unwindToWelcomeView segue called.")
         }
     }
-
+    
 }
