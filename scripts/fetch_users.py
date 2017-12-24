76cc0c2c56473a6ec386b44249717b7aa9a46282
Erik Phillips <erik.phillips@icloud.com>
2017-12-24 09:59:44 -0700

    added scripts; fetch users script

---
+++ scripts/fetch_users.py
@@ -0,0 +1,11 @@
+
+import pprint
+from firebase import firebase
+
+print "Fetching delegation-app users..."
+
+firebase = firebase.FirebaseApplication('https://delegation-app.firebaseio.com/', None)
+result = firebase.get('/users', None)
+
+print "Found {} users:".format(len(result))
+pprint.pprint(result)
