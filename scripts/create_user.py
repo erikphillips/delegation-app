41ed9227c2cf996ca29dff55637d70f822698b6c
Erik Phillips <erik.phillips@icloud.com>
2017-12-28 11:55:30 -0800

    added create users script and service account key

---
+++ scripts/create_user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# Copyright 2017 Erik Phillips
+# <COMMENT>
+
+# import pprint
+# from firebase import firebase
+
+# print "Fetching delegation-app users..."
+
+# firebase = firebase.FirebaseApplication('https://delegation-app.firebaseio.com/', None)
+# result = firebase.get('/users', None)
+
+# print "Found {} users:".format(len(result))
+# pprint.pprint(result)
+
+print "Tool not implemented."
