f0ffba99cc08b6e2bd8b9107955eaaa5f5303821
Erik Phillips <erik.phillips@icloud.com>
2018-03-05 19:22:32 -0800

    renamed iOS project and added macOS project

---
+++ DelegationApp/DelegationApp-macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macOSDelegationApp
+//
+//  Created by Erik Phillips on 3/5/18.
+//  Copyright © 2018 Erik Phillips. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DelegationApp/DelegationApp-macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macOSDelegationApp
+//
+//  Created by Erik Phillips on 3/5/18.
+//  Copyright © 2018 Erik Phillips. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
