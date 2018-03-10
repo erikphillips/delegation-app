9981f1df1acce2b37d76aad3dd7dd7e9b21dd3b2
Erik Phillips <erik.phillips@icloud.com>
2018-03-10 01:07:27 -0800

    added additional glyphs

---
+++ DelegationApp/DelegationApp-macOS/Controller/Main/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  DelegationApp-macOS
+//
+//  Created by Erik Phillips on 3/9/18.
+//  Copyright © 2018 Erik Phillips. All rights reserved.
+//
+
+import Cocoa
+
+class MainViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ DelegationApp/DelegationApp-macOS/Controller/Main/SideBarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SideBarViewController.swift
+//  DelegationApp-macOS
+//
+//  Created by Erik Phillips on 3/9/18.
+//  Copyright © 2018 Erik Phillips. All rights reserved.
+//
+
+import Cocoa
+
+class SideBarViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
