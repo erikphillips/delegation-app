0adbfe9a838bb8a2339b3ae639f9e6e48b976a74
Erik Phillips <erik.phillips@icloud.com>
2018-03-10 23:21:17 -0800

    added tableview delegation

--- DelegationApp/DelegationApp-macOS/Controller/Main/MainViewController.swift
+++ DelegationApp/DelegationApp-macOS/Controller/Main/MainViewController.swift
@@ -8,11 +8,37 @@
 
 import Cocoa
 
-class MainViewController: NSViewController {
+class MainViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate  {
 
+//    var tasks: [Task]?
+    var tasks: [String]? = ["One", "Two", "Three"]
+    
+    @IBOutlet weak var mainTableView: NSTableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         Logger.log("MainViewController viewDidLoad")
+        
+        self.mainTableView.delegate = self
+        self.mainTableView.dataSource = self
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        if let tasks = self.tasks {
+            return tasks.count
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView?{
+        var result: NSTableCellView
+        result  = self.mainTableView.makeView(withIdentifier: (tableColumn?.identifier)!, owner: self) as! NSTableCellView
+        
+        if let tasks = self.tasks {
+            result.textField?.stringValue = tasks[row]
+        }
+        
+        return result
     }
     
 }
--- DelegationApp/DelegationApp-macOS/Controller/Main/SideBarViewController.swift
+++ DelegationApp/DelegationApp-macOS/Controller/Main/SideBarViewController.swift
@@ -8,11 +8,37 @@
 
 import Cocoa
 
-class SideBarViewController: NSViewController {
+class SideBarViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
 
+//    var teams: [Team]?
+    var teams: [String]? = ["One", "Two", "Three"]
+    
+    @IBOutlet weak var sideTableView: NSTableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         Logger.log("SideBarViewController viewDidLoad")
+        
+        self.sideTableView.delegate = self
+        self.sideTableView.dataSource = self
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        if let teams = self.teams {
+            return teams.count
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView?{
+        var result: NSTableCellView
+        result  = self.sideTableView.makeView(withIdentifier: (tableColumn?.identifier)!, owner: self) as! NSTableCellView
+        
+        if let teams = self.teams {
+            result.textField?.stringValue = teams[row]
+        }
+        
+        return result
     }
     
 }
