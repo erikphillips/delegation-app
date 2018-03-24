58f76355488835c2a72302b37485a0f6851c169b
Erik Phillips <erik.phillips@icloud.com>
2018-03-23 23:45:27 -0700

    added example code

--- DelegationApp/DelegationApp-macOS/Model/ObservableNotifications.swift
+++ DelegationApp/DelegationApp-macOS/Model/ObservableNotifications.swift
@@ -6,6 +6,31 @@
 //  Copyright © 2018 Erik Phillips. All rights reserved.
 //
 
+/*
+ 
+ Example Notification Center Code
+ 
+ The following static property should be added to the top of the class:
+ static let notificationName = Notification.Name("myNotificationName")
+ 
+ Then the notification can be observed (added in the viewDidLoad method):
+ NotificationCenter.default.addObserver(self, selector: #selector(onNotification(notification:)), name: MainViewController.notificationName, object: nil)
+ 
+ To stop observing the notification:
+ NotificationCenter.default.removeObserver(self, name: MainViewController.notificationName, object: nil)
+ 
+ Or stop observing all notifications:
+ NotificationCenter.default.removeObserver(self)
+ 
+ Then the function (objective c) is called:
+ @objc func onNotification(notification:Notification) { print(notification.userInfo) }
+ 
+ To post the notification (notify all observers):
+ NotificationCenter.default.post(name: MainViewController.notificationName, object: nil, userInfo:["data": 42, "isImportant": true])
+ 
+ */
+
+
 class ObservableNotifications {
     
     static let NOTIFICATION_APP_LAUNCH: String = "NOTIFICATION_APP_LAUNCH"
