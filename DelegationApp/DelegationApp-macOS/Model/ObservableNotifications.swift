fe29f1fcbbc6113c17b1e35c6bc09d8d0e407bc9
Erik Phillips <erik.phillips@icloud.com>
2018-03-23 22:39:07 -0700

    notifications

---
+++ DelegationApp/DelegationApp-macOS/Model/ObservableNotifications.swift
@@ -0,0 +1,22 @@
+//
+//  ObservableNotifications.swift
+//  DelegationApp-macOS
+//
+//  Created by Erik Phillips on 3/23/18.
+//  Copyright © 2018 Erik Phillips. All rights reserved.
+//
+
+class ObservableNotifications {
+    
+    static let NOTIFICATION_APP_LAUNCH: String = "NOTIFICATION_APP_LAUNCH"
+    
+    static let NOTIFICATION_WINDOW_MAIN_OPEN: String = "NOTIFICATION_WINDOW_MAIN_OPEN"
+    static let NOTIFICATION_WINDOW_MAIN_CLOSE: String = "NOTIFICATION_WINDOW_MAIN_CLOSE"
+    
+    static let NOTIFICATION_SEGMENT_SELF: String = "NOTIFICATION_SEGMENT_SELF"
+    static let NOTIFICATION_SEGMENT_TEAM: String = "NOTIFICATION_SEGMENT_TEAM"
+    static let NOTIFICATION_SEGMENT_DELEGATION: String = "NOTIFICATION_SEGMENT_DELEGATION"
+    
+    static let NOTIFICATION_TEAM_SELECTION_PREFIX: String = "NOTIFICATION_TEAM_SELECTION_"
+    
+}
