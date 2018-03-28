9521e2387d9b53b6fd76d6604541bfe82966f55b
Erik Phillips <erik.phillips@icloud.com>
2018-03-27 17:42:58 -0700

    added selectable tables and view for changing task properties

--- DelegationApp/DelegationApp-macOS/Controller/Main/TaskDetail/TaskDetailSelectAssigneeViewController.swift
+++ DelegationApp/DelegationApp-macOS/Controller/Main/TaskDetail/TaskDetailSelectAssigneeViewController.swift
@@ -8,11 +8,97 @@
 
 import Cocoa
 
-class TaskDetailSelectAssigneeViewController: NSViewController {
+class TaskDetailSelectAssigneeViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
 
+    var users: [User]?
+    var task: Task?
+    var rootViewController: TaskDetailViewController?
+    
+    @IBOutlet weak var tableView: NSTableView!
+    @IBOutlet weak var loadingIndicator: NSProgressIndicator!
+    @IBOutlet weak var selectBtn: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        
+        Logger.log("TaskDetailSelectAssigneeViewController viewDidLoad")
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        self.loadingIndicator.startAnimation(nil)
+        self.tableView.isHidden = true
+        
+        self.tableView.doubleAction = #selector(TaskDetailSelectTeamViewController.doubleClickRow)
+
+        if let task = self.task {
+            let team = Team(guid: task.getTeamUID())
+            team.setupCallback = {
+                [team, weak self] in
+                guard let this = self else { return }
+                
+                this.users = []
+                
+                let dispatchGroup = DispatchGroup()
+                
+                for member in team.getMembers() {
+                    dispatchGroup.enter()
+                    let user = User(uuid: member)
+                    user.setupCallback = {
+                        [dispatchGroup] in
+                        dispatchGroup.leave()
+                    }
+                    
+                    this.users?.append(user)
+                }
+                
+                dispatchGroup.notify(queue: .main) {
+                    [weak this] in
+                    guard let that = this else { return }
+                    that.loadingIndicator.stopAnimation(nil)
+                    that.tableView.isHidden = false
+                    that.tableView.reloadData()
+                }
+            }
+        }
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        if let users = self.users {
+            return users.count
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let result = self.tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "GenericTableCell"), owner: self) as! GenericTableCellView
+        
+        if let users = self.users {
+            result.user = users[row]
+            result.textField?.stringValue = "\(users[row].getFullName()) (\(users[row].getEmailAddress()))"
+        }
+        
+        return result
+    }
+    
+    @objc func doubleClickRow() {
+        Logger.log("doubleClickRow \(self.tableView.clickedRow)")
+        self.selectBtnPressed(self)
+    }
+    
+    @IBAction func selectBtnPressed(_ sender: Any) {
+        let selectedRow = self.tableView.selectedRow
+        if selectedRow >= 0 {
+            if let user = self.users?[selectedRow] {
+                self.rootViewController?.selectedNewAssignee = user
+                self.rootViewController?.reloadViewElements()
+                self.dismiss(nil)
+            }
+        }
+    }
+    
+    @IBAction func cancelBtnPressed(_ sender: Any) {
+        self.dismiss(nil)
     }
     
 }
--- DelegationApp/DelegationApp-macOS/Controller/Main/TaskDetail/TaskDetailSelectTeamViewController.swift
+++ DelegationApp/DelegationApp-macOS/Controller/Main/TaskDetail/TaskDetailSelectTeamViewController.swift
@@ -8,11 +8,70 @@
 
 import Cocoa
 
-class TaskDetailSelectTeamViewController: NSViewController {
+class TaskDetailSelectTeamViewController: NSViewController,NSTableViewDelegate, NSTableViewDataSource  {
+    
+    var teams: [Team]?
+    var rootViewController: TaskDetailViewController?
 
+    @IBOutlet weak var tableView: NSTableView!
+    @IBOutlet weak var loadingIndicator: NSProgressIndicator!
+    @IBOutlet weak var selectBtn: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        self.tableView.doubleAction = #selector(TaskDetailSelectTeamViewController.doubleClickRow)
+
+        self.tableView.isHidden = true
+        self.loadingIndicator.startAnimation(nil)
+        FirebaseUtilities.fetchAllTeams { [weak self] (teams) in
+            guard let this = self else { return }
+            this.teams = teams
+            this.loadingIndicator.stopAnimation(nil)
+            this.tableView.isHidden = false
+            this.tableView.reloadData()
+        }
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        if let teams = self.teams {
+            return teams.count
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let result = self.tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "GenericTableCell"), owner: self) as! GenericTableCellView
+        
+        if let teams = self.teams {
+            result.team = teams[row]
+            result.textField?.stringValue = teams[row].getGUID()
+        }
+        
+        return result
+    }
+    
+    @objc func doubleClickRow() {
+        Logger.log("doubleClickRow \(self.tableView.clickedRow)")
+        self.selectBtnPressed(self)
+    }
+    
+    @IBAction func selectBtnPressed(_ sender: Any) {
+        let selectedRow = self.tableView.selectedRow
+        if selectedRow >= 0 {
+            if let team = self.teams?[selectedRow] {
+                self.rootViewController?.selectedNewTeam = team
+                self.rootViewController?.reloadViewElements()
+                self.dismiss(nil)
+            }
+        }
+    }
+    
+    @IBAction func cancelBtnPressed(_ sender: Any) {
+        self.dismiss(nil)
     }
     
 }
