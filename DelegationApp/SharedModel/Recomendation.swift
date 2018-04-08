9446c859e048b745806becbac0041adfd64dbc57
Erik Phillips <erik.phillips@icloud.com>
2018-04-08 10:42:24 -0700

    added recomendation class

---
+++ DelegationApp/SharedModel/Recomendation.swift
@@ -0,0 +1,24 @@
+//
+//  Recomendation.swift
+//  DelegationApp
+//
+//  Created by Erik Phillips on 4/8/18.
+//  Copyright © 2018 Erik Phillips. All rights reserved.
+//
+
+import Foundation
+import FirebaseCore
+import FirebaseAuth
+import FirebaseDatabase
+
+class Recomendation {
+    
+}
+
+private class UserSnapshot {
+    private var uuid: String
+    
+    init(uuid: String) {
+        self.uuid = uuid
+    }
+}
